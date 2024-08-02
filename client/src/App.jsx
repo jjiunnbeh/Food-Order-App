ee7328c77c920f9fa0f36e9f1497b8f29f06a4e6
jjiunnbeh <jjiunnbeh@gmail.com>
2024-08-02 22:18:43 +0800

    edit styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Header />
-      <div style={{ color: 'white' , fontSize: '3.5rem', textAlign: 'center'}}>OUR MENU</div>
+      <div style={{ color: 'white' , fontSize: '2rem', textAlign: 'center'}}>OUR MENU</div>
       <hr style={{marginLeft: '5%', marginRight:' 5%', opacity: 0.8, borderColor: 'lightgray'}}/>
     </>
   )
