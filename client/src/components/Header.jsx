dbbaeb1f7c87a71a6cbd16ebc4e8e8a2042c2fd0
jjiunnbeh <jjiunnbeh@gmail.com>
2024-08-02 21:32:09 +0800

    initial styling

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BsCart } from "react-icons/bs";
+
+const Header = () => {
+    const handleOpenCartClick = () => 
+    {
+
+    }
+
+
+    return (
+        <header>
+        <div >
+          <h1 id='restuarant-name-header'>RESTAURANT NAME</h1>
+        </div>
+         <button id='cart-icon' onClick={handleOpenCartClick}><BsCart /> (0)</button>
+      </ header>
+    );
+};
+
+export default Header;
