13fe8b9c0630e876d43f6a09907c3f32615e0013
jjiunnbeh <jjiunnbeh@gmail.com>
2024-08-03 03:51:46 +0800

    set up server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+const portNumber = 3000;
+app.listen(portNumber, ()=>
+{
+    console.log(`Server is listening at port ${portNumber}`);
+});
+
